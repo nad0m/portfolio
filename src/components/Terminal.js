97ef7c60f75239ae5d010bd0bfbc9a1fee340ded
Huy Nguyen <huyndinh@gmail.com>
2019-11-04 14:20:56 -0800

    Added terminal resize callback

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import '../styles/terminal.css';
 
-const Terminal = () => {
+class Terminal extends React.Component {
 
-    return (
-        <div className="terminal-container">
-            <div className="window-container">
-                <div className="window-controls">
-                    <button className="window-controls-btn close-btn"></button>
-                    <button className="window-controls-btn minimize-btn"></button>
-                    <button className="window-controls-btn expand-btn"></button>
-                </div>
-                <div className="window-content">
-                    <div>> Hello world</div>
-                    <div>> Hello world</div>
-                    <div>> Hello world</div>
-                    <div>> Hello world</div>
-                    <div>> Hello world</div>
+
+    state = {
+        clientWidth: null,
+        clientHeight: null
+    }
+
+    onWindowResize = () => {
+        const {clientWidth, clientHeight} = this.windowRef.current;
+        this.setState({ clientWidth, clientHeight });
+    }
+
+    constructor(props) {
+        super(props);
+        this.windowRef = React.createRef();
+        window.addEventListener('resize', this.onWindowResize);
+    }
+
+    componentDidMount() {
+        this.onWindowResize();
+    }
+
+    render() {
+        return (
+            <div className="terminal-container" ref={this.windowRef}>
+                <div className="window-container">
+                    <div className="window-controls">
+                        <button className="window-controls-btn close-btn"></button>
+                        <button className="window-controls-btn minimize-btn"></button>
+                        <button className="window-controls-btn expand-btn"></button>
+    
+                        <div className="window-description">Terminal -- {this.state.clientWidth}x{this.state.clientHeight}</div>
+                    </div>
+    
+                    <div className="window-content">
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                        <div>> Hello world</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Terminal;
