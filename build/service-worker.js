66ad51438d953f9f51ba9614f33fe46df81fb202
Adam Nguyen <adamdnguyen10@gmail.com>
2019-11-05 18:11:03 -0800

    Redeployed

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.49e2fa551651a189df4f4f04b4f97969.js"
+  "/portfolio/precache-manifest.2012b8cdca0c4771721ce5da189a0b50.js"
 );
 
 self.addEventListener('message', (event) => {
