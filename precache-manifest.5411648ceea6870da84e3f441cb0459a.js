83a9a2767e87b1b9c18820180719c5900c2a303e
Adam Nguyen <adamdnguyen10@gmail.com>
2019-11-06 00:07:00 -0800

    Updates

--- precache-manifest.5411648ceea6870da84e3f441cb0459a.js
+++ precache-manifest.5411648ceea6870da84e3f441cb0459a.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9e80340a80e18ed666d4d7d1b8cc3030",
+    "revision": "6fd16b455d1af3df1e2cd79cde4af8f0",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "ead9d7acd72911d1dce0",
+    "revision": "6fc4ca29cb643aea71ba",
     "url": "/portfolio/static/css/2.7eafa3a7.chunk.css"
   },
   {
-    "revision": "f22a7ad55f8024d5ecd1",
-    "url": "/portfolio/static/css/main.eac2b21f.chunk.css"
+    "revision": "a5b7d5d883e60f8bcff2",
+    "url": "/portfolio/static/css/main.45bd3f0c.chunk.css"
   },
   {
-    "revision": "ead9d7acd72911d1dce0",
-    "url": "/portfolio/static/js/2.9aa43ac9.chunk.js"
+    "revision": "6fc4ca29cb643aea71ba",
+    "url": "/portfolio/static/js/2.16eeffe7.chunk.js"
   },
   {
-    "revision": "f22a7ad55f8024d5ecd1",
-    "url": "/portfolio/static/js/main.0b8c1549.chunk.js"
+    "revision": "a5b7d5d883e60f8bcff2",
+    "url": "/portfolio/static/js/main.733850a6.chunk.js"
   },
   {
     "revision": "76aa8cbd67b5c937ec2c",
     "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "4af4a466f64bca11a8d0a633b4ad2ea9",
+    "url": "/portfolio/static/media/adamnguyen_resume.4af4a466.pdf"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
@@ -42,6 +46,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "4fc59a9f793ccd621bb9ac709a92a755",
+    "url": "/portfolio/static/media/cataclysm.4fc59a9f.jpg"
+  },
+  {
+    "revision": "cf6ed89221b2202fd471219734ebc02c",
+    "url": "/portfolio/static/media/fever.cf6ed892.png"
   },
   {
     "revision": "9c74e172f87984c48ddf5c8108cabe67",
@@ -68,6 +80,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/icons.faff9214.woff"
   },
   {
+    "revision": "fb446b56667925cea6a66f54d5169f16",
+    "url": "/portfolio/static/media/image-search.fb446b56.png"
+  },
+  {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
   },
@@ -86,5 +102,33 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "219860e1bdc98132f77714564526732b",
+    "url": "/portfolio/static/media/pomodoro.219860e1.jpg"
+  },
+  {
+    "revision": "9090835cd96bfec30dc2ebafa19d17bd",
+    "url": "/portfolio/static/media/portfolio.9090835c.png"
+  },
+  {
+    "revision": "4d5bd391aedefb01f5ac289dc7098ded",
+    "url": "/portfolio/static/media/simon.4d5bd391.jpg"
+  },
+  {
+    "revision": "d91e6c44173823cde27af8fca07b0049",
+    "url": "/portfolio/static/media/tictactoe.d91e6c44.jpg"
+  },
+  {
+    "revision": "a2b3616982ced576da95cce7d57a717c",
+    "url": "/portfolio/static/media/video-search.a2b36169.png"
+  },
+  {
+    "revision": "8fa7f49c6288a987e877035e7660078c",
+    "url": "/portfolio/static/media/vital.8fa7f49c.png"
+  },
+  {
+    "revision": "48a63d3dcc1effd1f3cd23b03c8c0c63",
+    "url": "/portfolio/static/media/wikipedia.48a63d3d.png"
   }
 ]);
