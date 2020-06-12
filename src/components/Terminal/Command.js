4254ba3f2a86534987ba1b82bbe5428a3beaba4b
Adam <adamdnguyen10@gmail.com>
2020-06-12 15:39:26 -0700

    add terminal

---
+++ src/components/Terminal/Command.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const parseOutput = (val) => {
+
+    if (typeof val === 'object') {
+        if (Array.isArray(val)) {
+            if (typeof val[0] === 'object') {
+                const objects = [];
+                for (let i = 0; i < val.length; i++) {
+                    objects.push('"');
+                    objects.push(parseObject(val[i]));
+                    objects.push('"');
+
+                    if (i !== val.length - 1)
+                        objects.push(", ");
+                }
+                return ["[", ...objects, "]"];
+            }
+            return toArray(val);
+        }
+        return ['"', parseObject(val), '"'];
+    }
+
+    return `"${val}"`;
+}
+
+const parseObject = (obj) => {
+    const keys = Object.keys(obj);
+    const values = Object.values(obj);
+
+    if (keys[0] === 'resume') {
+        return <a href='#' target="_blank" rel="noopener noreferrer" key={values[0]}>{values[0]}</a>
+    }
+        
+    return <a href={values[0]} target="_blank" rel="noopener noreferrer" key={keys[0]}>{keys[0]}</a>
+}
+
+const toArray = (arr) => {
+
+    const str = arr.reduce((acc, item) => {
+        if (acc.length === 0)
+            return `"${item}"`;
+
+        return `${acc}, "${item}"`;
+    }, "");
+
+    return `[${str}]`
+}
+
+const Wrapper = styled.div`
+  margin: 15px 0;
+`
+
+const Input = styled.div`
+  &::before {
+    content: ">";
+    margin: 0 5px 0 0;
+  }
+`
+
+const Output = styled.div`
+  color: #00f383;
+  &::before {
+    content: "";
+    margin: 0 0 0 1em;
+  }
+`
+
+const Command = ({ input, output }) => {
+    return (
+        <Wrapper>
+            <Input>{input}</Input>
+            <Output>{parseOutput(output)}</Output>
+        </Wrapper>
+    );
+}
+
+export default Command;
--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -1,39 +1,89 @@
 import React from 'react';
-import './terminal.css';
-import './typewriter.css';
+import styled, { keyframes } from 'styled-components'
 
-class Terminal extends React.Component {
+import Command from './Command'
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  font-family: 'Roboto Mono', monospace;
+  border-radius: 7px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  width: 100%;
+`
 
-    constructor(props) {
-        super(props);
-        window.addEventListener('resize', this.onWindowResize);
-    }
+const Window = styled.div`
+  width: 100%;
+  height: auto;
+  border: 1px solid #bbb;
+  background: #1e1f29;
+  border-radius: 7px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`
 
-    render() {
+const ButtonsContainer = styled.div`
+  display: flex;
+  padding: 5px;
+  background: linear-gradient(rgb(226, 224, 226), rgb(199, 198, 200));
+`
 
-        return (
-            <div className="terminal-container">
-                <div className="window-container">
-                    <div className="window-controls">
-                        <button className="window-controls-btn close-btn"></button>
-                        <button className="window-controls-btn minimize-btn"></button>
-                        <button className="window-controls-btn expand-btn"></button>
-    
-                        <div className="window-description">Terminal</div>
-                    </div>
-    
-                    <div className="window-content">
-                        <div className="typewriter-body-span">
-                            <div className="typewriter-span">
-                                <span>> {}</span>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+const WindowContent = styled.div`
+  display: flex;
+  padding: 0 20px 20px;
+  flex-direction: column;
+  flex: 0 0 95%;
+  background: #1e1f29;
+  color: #fff;
+`
+
+const TypeWriterBody = styled.div`
+  margin: 15px;
+`
+
+const blinkCaret = keyframes`
+  from, to { border-color: transparent }
+  50% { border-color: #fff }
+`
+
+const TypeWriter = styled.div`
+  display: inline;
+  color: #fff;
+  font-family: 'Monaco', Consolas, 'Lucida Console', monospace;
+  overflow: hidden;
+  border-right: .50em solid #fff;
+  white-space: nowrap;
+  letter-spacing: .50em; 
+  animation: ${blinkCaret} .75s step-end infinite;
+
+  &::before {
+    content: ">"
+  }
+`
+
+const Terminal = () => {
+    return (
+        <Wrapper>
+            <Window>
+                <ButtonsContainer>
+                    hello
+                </ButtonsContainer>
+                <WindowContent>
+                    <TypeWriterBody>
+                        <Command input="Input" output="Output" />
+                        <Command input="Input" output="Output" />
+                        <Command input="Input" output="Output" />
+                        <Command input="Input" output="Output" />
+                        <Command input="Input" output="Output" />
+                        <TypeWriter />    
+                    </TypeWriterBody>
+                </WindowContent>
+            </Window>
+        </Wrapper>
+    )
 }
 
 export default Terminal;
