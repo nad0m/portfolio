c3aa0bb44ef3f610fe70a1ac4bce4ff445682b2a
Adam <adamdnguyen10@gmail.com>
2020-06-15 03:02:38 -0700

    add tracker

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.995039032c4890ea55dfea3d95e1ea66.js"
+  "/portfolio/precache-manifest.b5c3cdab516b2fcd1a798f487fed2c6c.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 import Home from './pages/Home'
+import useTracker from './utils/useTracker'
 import portfolio from './config/portfolio-data.json'
 
 function App() {
+  useTracker()
+  
   return (
     <Home data={portfolio} />
   )
